afa70a33c099173b0b05299bc306311e369c1f2b
csteamengine <gcs@iastate.edu>
2015-12-06 18:59:55 +0000

    Added a bunch of code to make motors light up when left joystick is pushed up or to the side.

--- Code.py
+++ Code.py
@@ -1,29 +1,23 @@
 import time
 import serial
 import pygame
+import RPi.GPIO as GPIO
 
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+motor1 = GPIO.PWM(4,100)
+motor1.start(0)
+GPIO.setup(17, GPIO.OUT)
+motor2 = GPIO.PWM(17,100)
+motor2.start(0)
 pygame.init()
 pygame.joystick.init()
-ser = serial.Serial('/dev/ttyACM0',9600)
 clock = pygame.time.Clock()
-Side = False
-Forw = False
 done = False
-TiltAmount = 50
-AutoPilot = False
-#StartEngines = False
-Rup = 0
-Lright = 0
-YawLeft = 0
-Lleft = 0
-hover = 1000
-time.sleep(10)
-YawRight = 0
-Lfor = 0
-Lback = 0
- print("Script Loaded")
- while done == False:
+LMA = 0
+RMA = 0
+print("Script Loaded")
+while done == False:
     for event in pygame.event.get():
          if event.type == pygame.QUIT:
                  done = True
@@ -43,98 +37,23 @@ Lback = 0
     Select = controller.get_button(0)
     PS = controller.get_button(16)
     X = controller.get_button(14)
+    if LU <= 0:
+        LMA = 20 * abs(LU)
+        RMA = 20 * abs(LU)
+        if LS < 0:
+            RMA += 10 * abs(LS)
+        if LS > 0:
+            LMA += 10 * abs(LS)
 
-
-    if AutoPilot == False:# and StartEngines ==True:
-         if RU < 0:
-             Rup = (2000 - hover) * abs(RU)
-         elif RU > 0:
-             Rup = 0 - (hover - 800) * abs(RU)
-         else:
-             Rup = 0
-
-         if RS < 0:
-             YawLeft = 0- TiltAmount*abs(RS)
-         elif RS > 0:
-             YawRight =0- TiltAmount*abs(RS)
-         else:
-             YawRight = 0
-             YawLeft = 0
-
-         if LU < 0:
-             Lfor =0- TiltAmount*abs(LU)
-             Forw = True
-             Lback =0
-         elif LU > 0:
-             Lback = 0-TiltAmount*abs(LU)
-             Forw = True
-             Lfor = 0
-         else:
-             Lfor = 0
-             Lback = 0
-             Forw = False
-
-         if LS < 0:
-             Lright =0- TiltAmount*abs(LS)
-             Side = True
-             Lleft = 0
-         elif LS > 0:
-             Lleft = 0-TiltAmount*abs(LS)
-             Side = True
-             Lright = 0
-         else:
-             Lleft = 0
-             Lright = 0
-             Side = False
-
-         #if Side == False:
-                 #if GYSide > 0:
-                     #GyLeft = TiltAmount*GYSide
-                 #if GYSide < 0:
-                     #GyRight = TiltAmount*abs(GYSide)
-                 #if GYSide == 0:
-                     #GyRight = 0
-                     #GyLeft = 0
-         #if Front == False:
-             #if GYFor <0:
-                     #GyFront = TiltAmount*abs(GYFor)
-             #if GYFor >0:
-                     #GyBack = TiltAmount*GYFor
-             #if GYFor == 0:
-                     #GyFront = 0
-                     #GyBack = 0
-    RMA = hover + Rup + Lleft + YawLeft#  +GyRight
-    LMA = hover + Rup + Lright + YawLeft# + GyLeft
-    BMA = hover + Rup + Lback + YawRight# + GyBack
-    FMA = hover + Rup + Lfor + YawRight#  + GyFront
-    if AutoPilot == True:
-         RMA = hover # + gyro
-         FMA = hover # + gyro
-         LMA = hover # + gyro
-         BMA = hover # + gyro
-    ser.write(str(FMA) + ',' + str(BMA) + ',' + str(BMA) + ',' + str(LMA))
+    motor1.ChangeDutyCycle(RMA)
+    motor2.ChangeDutyCycle(LMA)
     if L1 ==1 and R1 == 1 and R2 == 1 and L2 ==1:
          done = True
          print("Done!")
-         ser.write('700,700,700,700')
-    if Up == 1:
-         if hover < 2000:
-             hover = hover + 50
-             print("Hover Increased: {}".format(hover))
-    if Down == 1:
-         if hover >= 750:
-             hover = hover - 50
-             print("Hover Decreased")
-    if Select == 1:
-         AutoPilot = False
-         print("AutoPilot Disengaged")
-    if PS == 1:
-         AutoPilot = True
-         print("AutoPilot Engaged")
-    #if Start ==1:
-         #StartEngines == True
     if X == 1:
-         hover = 1000
-         print("Hover reset: 1000")
+        print "BEE BEE DOH"
     clock.tick(30)
+motor1.stop()
+motor2.stop()
+GPIO.cleanup()
 pygame.quit()
